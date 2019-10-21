e1f28d00a387f0ac0029419197eb2ebfeaa6733e
Linh Pham <question+github@closedsrc.org>
2019-10-20 22:20:33 -0700

    Continuing work on template, added CSS and getting data dicts/lists sorted out

--- run.py
+++ run.py
@@ -14,16 +14,45 @@ import pytz
 
 from jinja2 import Environment, FileSystemLoader
 
-def retrieve_appearance_counts(database_connection: mysql.connector.connect
-                              ) -> Dict:
+def retrieve_panelist_appearance_counts(panelist_id: int,
+                                        database_connection: mysql.connector.connect
+                                       ) -> List[Dict]:
+    """Retrieve yearly apperance count for the requested panelist ID"""
+
+    cursor = database_connection.cursor()
+    query = ("SELECT YEAR(s.showdate) AS year, COUNT(p.panelist) AS count "
+             "FROM ww_showpnlmap pm "
+             "JOIN ww_shows s ON s.showid = pm.showid "
+             "JOIN ww_panelists p ON p.panelistid = pm.panelistid "
+             "WHERE pm.panelistid = %s AND s.bestof = 0 "
+             "AND s.repeatshowid IS NULL "
+             "GROUP BY p.panelist, YEAR(s.showdate) "
+             "ORDER BY p.panelist ASC, YEAR(s.showdate) ASC")
+    cursor.execute(query, (panelist_id, ))
+    result = cursor.fetchall()
+
+    if not result:
+        return None
+
+    appearances = OrderedDict()
+    total_appearances = 0
+    for row in result:
+        appearances[row[0]] = row[1]
+        total_appearances += row[1]
+
+    appearances["total"] = total_appearances
+    return appearances
+
+def retrieve_all_panelist_appearance_counts(database_connection: mysql.connector.connect
+                                           ) -> List[Dict]:
     """Retrieve all appearance counts for all panelists from the
     database"""
 
     cursor = database_connection.cursor()
     query = ("SELECT DISTINCT p.panelistid, p.panelist "
-             "FROM ww_panelists p "
-             "JOIN ww_showpnlmap pm ON pm.panelistid = p.panelistid "
-             "JOIN ww_shows s ON s.showid = pm.panelistid "
+             "FROM ww_showpnlmap pm "
+             "JOIN ww_panelists p ON p.panelistid = pm.panelistid "
+             "JOIN ww_shows s ON s.showid = pm.showid "
              "WHERE s.bestof = 0 AND s.repeatshowid IS NULL "
              "ORDER BY p.panelist ASC")
     cursor.execute(query)
@@ -32,38 +61,17 @@ def retrieve_appearance_counts(database_connection: mysql.connector.connect
     if not result:
         return None
 
-    panelist_appearances = []
+    panelists = []
     for row in result:
-        panelist = OrderedDict()
-        panelist["name"] = row[0]
-        panelist["appearances"] = []
-        panelist_appearances.append(panelist)
+        panelist = {}
+        panelist_id = row[0]
+        panelist["name"] = row[1]
+        appearances = retrieve_panelist_appearance_counts(panelist_id,
+                                                          database_connection)
+        panelist["appearances"] = appearances
+        panelists.append(panelist)
 
-    cursor = database_connection.cursor(dictionary=True)
-    query = ("SELECT p.panelist, YEAR(s.showdate) AS year, "
-             "COUNT(p.panelist) AS count FROM ww_showpnlmap pm "
-             "JOIN ww_shows s ON s.showid = pm.showid "
-             "JOIN ww_panelists p ON p.panelistid = pm.panelistid "
-             "WHERE s.bestof = 0 AND s.repeatshowid IS NULL "
-             "GROUP BY p.panelist, YEAR(s.showdate) "
-             "ORDER BY p.panelist ASC, YEAR(s.showdate) ASC")
-    cursor.execute(query)
-    result = cursor.fetchall()
-    cursor.close()
-
-    if not result:
-        return None
-
-    for row in result:
- 
-
-
-        appearance_year = OrderedDict()
-        appearance_year["year"] = row["year"]
-        appearance_year["count"] = row["count"]
-        appearance_counts[panelist].append(appearance_year)
-
-    return appearance_counts
+    return panelists
 
 def retrieve_all_years(database_connection: mysql.connector.connect) -> List[int]:
     """Retrieve a list of all available show years"""
@@ -105,7 +113,7 @@ def load_config():
     return config_dict
 
 def render_report(show_years: List[int],
-                  appearance_counts: Dict,
+                  panelists: List[Dict],
                   ga_property_code: Text) -> Text:
     """Render appearances report using Jinja2"""
 
@@ -124,7 +132,7 @@ def render_report(show_years: List[int],
     # Build dictionary to pass into template renderer
     render_data = {}
     render_data["show_years"] = show_years
-    render_data["appearance_counts"] = appearance_counts
+    render_data["panelists"] = panelists
     render_data["ga_property_code"] = ga_property_code
     render_data["rendered_at"] = rendered_date_time.strftime("%A, %B %d, %Y %H:%M:%S %Z")
 
@@ -138,11 +146,11 @@ def main():
 
     app_config = load_config()
     database_connection = mysql.connector.connect(**app_config["database"])
-    appearance_counts = retrieve_appearance_counts(database_connection)
+    panelists = retrieve_all_panelist_appearance_counts(database_connection)
     show_years = retrieve_all_years(database_connection)
 
     rendered_report = render_report(show_years,
-                                    appearance_counts,
+                                    panelists,
                                     app_config["google_analytics_property_code"])
     print(rendered_report)
 
